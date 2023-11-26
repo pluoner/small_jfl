5556935b87dac1335a6e502ef8d79d26c783614c
pluoner <jonathan.gerholm@gmail.com>
2023-11-26 22:36:57 +0100

    -

--- AdventOfCode2022/Dec19/Dec19.java
+++ AdventOfCode2022/Dec19/Dec19.java
@@ -14,14 +14,15 @@ public class Dec19 {
 class RobotFactory {
     ArrayList<Blueprint> blueprints;
     RobotFactory(ArrayList<String> inputBluePrints) {
+        blueprints = new ArrayList<>();
         for (String s : inputBluePrints) {
             blueprints.add(new Blueprint(s));
         }
     }
 }
 class Blueprint {
-    int id;
-    int[] oreRobotCost, clayRobotCost, obsidianRobotCost, geodeRobotCost;
+    private int id, maxNoGeodes;
+    private int[] oreRobotCost, clayRobotCost, obsidianRobotCost, geodeRobotCost;
     Blueprint(String s) {
         String[] sa = s.split(":");
         id = Integer.parseInt(sa[0].replaceAll("Blueprint ", ""));
@@ -31,8 +32,40 @@ class Blueprint {
         obsidianRobotCost = parseRobotCost(rc[2]);
         geodeRobotCost = parseRobotCost(rc[3]);
     }
+    
+    void calcQualityLevel() {
+        
+    }
+
+    int getQualityLevel() {
+        return id * maxNoGeodes;
+    }
+
     private int[] parseRobotCost(String costRow) {
-        int[] ia = new int[4];
+        int[] ia = new int[3];
+        int st = -1;
+        for (int i = 0; i < costRow.length(); i++) {
+            if (Character.isDigit(costRow.charAt(i))) {
+                if (st == -1) {
+                    st = i;
+                }
+             } else if (i > 0 && Character.isDigit(costRow.charAt(i-1))) {
+                int cost = Integer.parseInt(costRow.substring(st, i));
+                switch(costRow.substring(i + 1, i + 3)) {
+                    case "or":
+                        ia[0] = cost;
+                        break;
+                    case "cl":
+                        ia[1] = cost;
+                        break;
+                    case "ob":
+                        ia[2] = cost;
+                        break;
+                }
+                st = -1;
+                i = i + 3;
+            }
+        }
         return ia;
     }
 };
