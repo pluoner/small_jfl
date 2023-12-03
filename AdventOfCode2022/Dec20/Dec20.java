d1c6b4c3efd363d914f3600e602f0590973444ff
pluoner <jonathan.gerholm@gmail.com>
2023-12-03 17:44:58 +0100

    Working solution part 1

--- AdventOfCode2022/Dec20/Dec20.java
+++ AdventOfCode2022/Dec20/Dec20.java
@@ -7,51 +7,56 @@ import AdventOfCode2022.Common.Helpers;
 public class Dec20 {
     public static void main(String[] args) {
         ArrayList<String> input = Helpers.imp("Dec20/res/input.txt");
-        ArrayList<Integer> encryptedFile = new ArrayList<>();
+        ArrayList<Integer[]> encryptedFile = new ArrayList<>();
         for (String s : input) {
-            encryptedFile.add(Integer.parseInt(s));
+            encryptedFile.add(new Integer[] {Integer.parseInt(s), 0, null});
         }
-        mixList(encryptedFile);
-        int zeroIx = 0;
-        for (int i = 0; i < encryptedFile.size(); i++) {
-            if (encryptedFile.get(i) == 0) {
-                zeroIx = i;
-                break;
-            }
+        //part 1:
+        // mixList(encryptedFile);
+        // int zeroIx = 0;
+        // for (int i = 0; i < encryptedFile.size(); i++) {
+        //     if (encryptedFile.get(i)[0] == 0) {
+        //         zeroIx = i;
+        //         break;
+        //     }
+        // }
+        // int sum = 0;
+        // for (int i = 0; i < 3; i++) {
+        //     sum += encryptedFile.get((zeroIx + (1000 * (i + 1))) % encryptedFile.size())[0];
+        // }
+        // System.out.println();
+        // System.out.println("Sum of values at 1000, 2000 and 3000 is: " + sum);
+
+        //part 2:
+        int decryptKey = 811589153;
+        for (Integer[] ia : encryptedFile) {
+            ia[0] *= decryptKey;
         }
-        int sum = 0;
-        for (int i = 0; i < 3; i++) {
-            sum += encryptedFile.get((zeroIx + (1000 * (i + 1))) % encryptedFile.size()); //todo
+        for (int i = 0; i < 10; i++) {
+            mixList(encryptedFile);
         }
-        System.out.println("Sum of values at 1000, 2000 and 3000 is: " + sum);
     }
 
-    private static void mixList(ArrayList<Integer> encryptedFile) {
-        ArrayList<Integer> movedIdx = new ArrayList<>();
+    private static void mixList(ArrayList<Integer[]> encryptedFile) {
         int curVal = 0;
         int nidx = 0;
-        for (int idx = 0; idx < encryptedFile.size(); idx++) {
-            if (movedIdx.contains(idx)) {
+        for (int idx = 0; idx < encryptedFile.size();) {
+            if (encryptedFile.get(idx)[1] == 1) {
+                idx++;
                 continue;
             }
-            curVal = encryptedFile.get(idx);
-            nidx = idx + curVal % encryptedFile.size();
+            curVal = encryptedFile.get(idx)[0];
+            encryptedFile.remove(idx);
+            nidx = idx + curVal;
+            if (nidx >= encryptedFile.size() || -nidx >= encryptedFile.size()) {
+                nidx = nidx % encryptedFile.size();
+            }
             if (nidx < 0) {
                 nidx += encryptedFile.size();
             }
-            if (nidx > encryptedFile.size()) {
-                nidx = nidx % encryptedFile.size();
-            }
-            encryptedFile.remove(idx);
-            encryptedFile.add(nidx, curVal);
-            for (int ix : movedIdx) {
-                if (nidx >= ix) {
-                    ix--;
-                }
-            }
-            movedIdx.add(nidx);
-            if (nidx > idx) {
-                idx--; // to counter loop iteration to next idx
+            encryptedFile.add(nidx, new Integer[] {curVal, 1});
+            if (nidx <= idx) {
+                idx++;
             }
         }
     }
