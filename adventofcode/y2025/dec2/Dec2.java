30f106c0585515e719fa34bb6ae45f88ef097e82
Jonathan Gerholm <jg@kase.se>
2025-12-10 10:13:16 +0100

    wip

--- adventofcode/y2025/dec2/Dec2.java
+++ adventofcode/y2025/dec2/Dec2.java
@@ -9,7 +9,7 @@ import adventofcode.common.Helpers;
 public class Dec2 {
     private static final Logger LOGGER = Logger.getLogger(Dec2.class.getName());
     public static void main(String[] args) {
-        List<String> input = Helpers.imp("y2025/dec2/res/input.txt");
+        List<String> input = Helpers.imp("y2025/dec2/res/testinput.txt");
         List<Long[]> intervals = new ArrayList<>();
         for (String line : input) {
             String[] ints = line.split(",");
@@ -23,21 +23,42 @@ public class Dec2 {
         }
         Integer totSilly = 0;
         for (Long[] interval : intervals) {
-            Long nSilly = nextSilly(interval[0]);
+            Long nSilly = nextSilly(interval[0], false);
             while (true) {
                 if (nSilly > interval[1]) {
                     break;
                 }
                 totSilly++;
-                nSilly = nextSilly(nSilly);
+                nSilly = nextSilly(nSilly, true);
             }
-            LOGGER.log(Level.INFO, "Interval: {0,number,#}-{1,number,#}", new Object[]{interval[0], interval[1]});
         }
-        // LOGGER.log(Level.INFO, "On zero: {0,number,#}", atzeroCounter);
-        // LOGGER.log(Level.INFO, "Passed zero: {0,number,#}", passedZeroCounter + atzeroCounter);
+        LOGGER.log(Level.INFO, "Silly count: {0,number,#}", totSilly);
     }
-    private static Long nextSilly(Long n) {
-        
-        return null;
+    private static Long nextSilly(Long n, boolean knownSilly) {
+        int digits = noDigits(n);
+        int sillyExp = (digits + 1) / 2;
+        Long sillyBase = n / 10^sillyExp;
+        if (knownSilly) {
+            sillyBase++;
+        }
+        while (true) {
+            Long candidate = buildSilly(sillyBase, sillyExp);
+            if (candidate > n) {
+                return candidate;
+            }
+            sillyBase++;
+        }
+    }
+
+    private static Long buildSilly(Long sillyBase, int sillyExp) {
+        return sillyBase * 10^sillyExp + sillyBase;
+    }
+    private static int noDigits(Long n) {
+        int digits = 0;
+        while (n > 0) {
+            n /= 10;
+            digits++;
+        }
+        return digits;
     }
 }
